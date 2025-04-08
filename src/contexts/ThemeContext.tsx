a92a494a913647de8849559e26ad9953a75e2540
TheOnly1TY <anifowosetemitayo5@gmail.com>
2025-04-08 15:19:25 -0700

    fixed bug

--- src/contexts/ThemeContext.tsx
+++ src/contexts/ThemeContext.tsx
@@ -1,33 +1,45 @@
 import { createContext, ReactNode, useContext, useEffect, useState } from "react";
 
+type ThemeContextType = {
+  theme: "light" | "dark";
+  handleThemeSwitch: () => void;
+};
 
 const ThemeContext = createContext<ThemeContextType>({
-    theme: "light",
-    handleThemeSwitch: () => {}
-})
-type ThemeContextType = {theme: "light" | "dark", handleThemeSwitch: () => void}
+  theme: "light",
+  handleThemeSwitch: () => {},
+});
 
-    
+function ThemeProvider({ children }: { children: ReactNode }) {
+  const [theme, setTheme] = useState<"light" | "dark">(() => {
+    const storedTheme = localStorage.getItem("Theme");
+    return storedTheme === "dark" ? "dark" : "light";
+  });
 
- function ThemeProvider({children}: {children: ReactNode}){
-    const [theme, setTheme] = useState<"light" | "dark">("light")
-    const handleThemeSwitch = () =>{
-      setTheme((prev) => prev === "light" ? "dark" : 'light')
-    }
-    useEffect(function(){
-      document.body.classList.toggle("dark", theme === "dark");
-  
-    }, [theme])
+  const handleThemeSwitch = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+    localStorage.setItem("Theme", newTheme);
+  };
 
-    return <ThemeContext.Provider value={{handleThemeSwitch, theme}}>
-        {children}
+  useEffect(() => {
+    document.body.classList.toggle("dark", theme === "dark");
+  }, [theme]);
+
+  return (
+    <ThemeContext.Provider value={{ handleThemeSwitch, theme }}>
+      {children}
     </ThemeContext.Provider>
-}
-function UseTheme(){
-    const context = useContext(ThemeContext)
-    if(context === undefined) {throw new Error("ThemeContext must be used within the ThemeProvider")}
-
-    return context
+  );
 }
 
-export {ThemeProvider, UseTheme}
+function UseTheme() {
+  const context = useContext(ThemeContext);
+  if (context === undefined) {
+    throw new Error("ThemeContext must be used within the ThemeProvider");
+  }
+
+  return context;
+}
+
+export { ThemeProvider, UseTheme };
